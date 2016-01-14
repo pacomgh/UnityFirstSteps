c334c97686984be6662b557cd24533b36d4891d9
Francisco Guzmán <paco.mgh@hotmail.com>
2016-01-13 18:21:07 -0600

    create and move sphere

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate (Input.GetAxis("Horizontal")*speed*Time.deltaTime,
+                            0,
+                            Input.GetAxis("Vertical")*speed*Time.deltaTime);
+	
+	}
+}
